ec360d64632f159665a2c70dfece1066297e1fbe
Oleh <berolegnik@gmail.com>
2025-03-05 19:47:54 +0200

    header

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,36 +1,38 @@
 import React from 'react'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
-import Home from './pages/Home' // Импорт компонента Home
-import Navbar from './pages/Navbar' // Импортируем Navbar
-import Categories from './pages/Categories' // Импорт компонента Categories
-import Products from './pages/Products' // Импорт компонента Products
-import Cart from './pages/Cart' // Импорт компонента Cart
-import Register from './pages/Register' // Импорт компонента Register
-import ProductDetails from './pages/ProductDetails' // Импорт компонента для деталей продукта
-import CategoryPage from './pages/CategoryPage' // Импорт компонента для отображения товаров в категории
+import Home from './pages/Home'
+import Navbar from './components/Navbar'
+import Banner from './components/Banner'
+import Categories from './pages/Categories'
+import Products from './pages/Products'
+import Cart from './pages/Cart'
+import Register from './pages/Register'
+import ProductDetails from './pages/ProductDetails'
+import CategoryPage from './pages/CategoryPage'
+import Footer from './components/Footer'
+import './App.css'
 
 function App() {
   return (
     <Router>
-      {/* Навигационная панель, которая будет отображаться на всех страницах */}
-      <Navbar />
-      {/* Основной контейнер для всего контента */}
-      <Routes>
-        {/* Главная страница */}
-        <Route path="/" element={<Home />} />
-        {/* Страница категорий */}
-        <Route path="/categories" element={<Categories />} />
-        {/* Страница продуктов */}
-        <Route path="/products" element={<Products />} />
-        {/* Страница корзины */}
-        <Route path="/cart" element={<Cart />} />
-        {/* Страница регистрации */}
-        <Route path="/register" element={<Register />} />
-        {/* Страница с деталями продукта */}
-        <Route path="/product/:id" element={<ProductDetails />} />
-        {/* Страница товаров по категории */}
-        <Route path="/category/:categoryId" element={<CategoryPage />} /> {/* Добавляем новый маршрут для категории */}
-      </Routes>
+      <div className="app">
+        <header className="header-container">
+          <Navbar />
+          <Banner /> {/* Баннер отображается только на главной странице */}
+        </header>
+        <main className="content">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/categories" element={<Categories />} />
+            <Route path="/products" element={<Products />} />
+            <Route path="/cart" element={<Cart />} />
+            <Route path="/register" element={<Register />} />
+            <Route path="/product/:id" element={<ProductDetails />} />
+            <Route path="/category/:categoryId" element={<CategoryPage />} />
+          </Routes>
+        </main>
+        <Footer />
+      </div>
     </Router>
   )
 }
