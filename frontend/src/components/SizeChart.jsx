aa10855fe3e2e5b8055a8e5aa55e3b8a4bd8b741
Oleh <berolegnik@gmail.com>
2025-03-15 13:58:15 +0200

    work style mobile

--- frontend/src/components/SizeChart.jsx
+++ frontend/src/components/SizeChart.jsx
@@ -7,25 +7,21 @@ const SizeChart = ({ selectedSetSize }) => {
       sheet: '160x220 см - 1 шт',
       duvetCover: '150x220 см - 1 шт',
       pillowcases: '70x70 см или 50x70 см - 2 шт',
-      additional: 'Пошиття на резинці +150 грн (повна передоплата)',
     },
     '2сп': {
       sheet: '200x220 см - 1 шт',
       duvetCover: '180x220 см - 1 шт',
       pillowcases: '70x70 см или 50x70 см - 2 шт',
-      additional: 'Пошиття на резинці +150 грн (повна передоплата)',
     },
     Євро: {
       sheet: '220x220 см - 1 шт',
       duvetCover: '200x220 см - 1 шт',
       pillowcases: '70x70 см или 50x70 см - 2 шт',
-      additional: 'Пошиття на резинці +150 грн (повна передоплата)',
     },
     Сімейний: {
       sheet: '220x220 см - 1 шт',
       duvetCover: '150x220 см - 2 шт',
       pillowcases: '70x70 см или 50x70 см - 2 шт',
-      additional: 'Пошиття на резинці +150 грн (повна передоплата)',
     },
   })
 
@@ -41,8 +37,6 @@ const SizeChart = ({ selectedSetSize }) => {
 
   return (
     <div className={styles.sizeChart}>
-      {/* <h2>Таблица размеров комплекта постельного белья</h2> */}
-
       <table className={styles.table}>
         <thead>
           <tr>
@@ -50,7 +44,6 @@ const SizeChart = ({ selectedSetSize }) => {
             <th>Простирадло</th>
             <th>Подковдра</th>
             <th>Наволочки</th>
-            <th>Дополнительно</th>
           </tr>
         </thead>
         <tbody>
@@ -59,7 +52,6 @@ const SizeChart = ({ selectedSetSize }) => {
             <td>{currentSizeData.sheet}</td>
             <td>{currentSizeData.duvetCover}</td>
             <td>{currentSizeData.pillowcases}</td>
-            <td>{currentSizeData.additional}</td>
           </tr>
         </tbody>
       </table>
