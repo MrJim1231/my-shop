dc1c9f93a8ae8e35521f97a330c3a94982bf0277
Oleh <berolegnik@gmail.com>
2025-03-19 20:58:15 +0200

    fix galery 1 img

--- frontend/src/components/ImageGallery.jsx
+++ frontend/src/components/ImageGallery.jsx
@@ -1,22 +1,44 @@
-import React from 'react'
+import React, { useState } from 'react'
 import styles from '../styles/ImageGallery.module.css' // Импортируем новый файл стилей
 
-const ImageGallery = ({ images, setPreviousImage, previousImage }) => {
-  // Обрезаем массив изображений, если их больше 5
-  const displayedImages = images.length > 5 ? images.slice(0, 5) : images
+const ImageGallery = ({ images }) => {
+  const [previousImage, setPreviousImage] = useState(null)
+
+  // Убираем повторяющиеся изображения
+  const uniqueImages = [...new Set(images)]
+
+  // Если картинок больше 5, обрезаем массив
+  const displayedImages = uniqueImages.length > 5 ? uniqueImages.slice(0, 5) : uniqueImages
+
+  // Если картинок больше одной, показываем миниатюры
+  const showThumbnails = displayedImages.length > 1
+
+  // Если картинок только одна, показываем только её
+  const mainImage = previousImage || displayedImages[0]
 
   return (
     <div className={styles.productInfo}>
-      {displayedImages && displayedImages.length > 1 && (
+      {showThumbnails && (
         <div className={styles.thumbnailContainer}>
           {displayedImages.map((image, index) => (
-            <img key={index} src={image} alt={`Product Image ${index + 1}`} className={styles.thumbnailImage} onClick={() => setPreviousImage(image)} />
+            <img
+              key={index}
+              src={image}
+              alt={`Product Image ${index + 1}`}
+              className={styles.thumbnailImage}
+              onClick={() => setPreviousImage(image)} // Изменение основного изображения при клике
+            />
           ))}
         </div>
       )}
 
+      {/* Основное изображение */}
       <div className={styles.mainImage}>
-        {previousImage ? <img src={previousImage} alt="Main Product Image" className={styles.mainImageDisplay} /> : <div className={styles.noImage}>Изображения отсутствуют</div>}
+        <img
+          src={mainImage} // Показываем основное изображение
+          alt="Main Product Image"
+          className={styles.mainImageDisplay}
+        />
       </div>
     </div>
   )
