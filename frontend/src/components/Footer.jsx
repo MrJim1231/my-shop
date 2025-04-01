ded3d62e32e9c156899dcc850c1e2fc208ce9a03
Oleh <berolegnik@gmail.com>
2025-04-01 21:32:02 +0300

    footer

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -6,7 +6,7 @@ const Footer = () => {
     <footer className={styles.footer}>
       <div className={styles.footerContent}>
         <p>&copy; 2025 Магазин постільної білизни. Усі права захищені.</p>
-        <p>Контакт: info@beddingstore.com | Телефон: +38 067 123 4567</p>
+        <p>Контакт: berolegnik@gmail.com | Телефон: +380975566344</p>
       </div>
     </footer>
   )
